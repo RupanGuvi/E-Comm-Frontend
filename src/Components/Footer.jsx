6fa71a5c92c09672664d45a6ff7a19360a7d5924
RupanGuvi <rupanguvi@gmail.com>
2024-09-30 11:42:22 +0530

    get completed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-           <footer className='py-5 bg-dark'>
+           <footer className='py-3 bg-dark'>
             <div className='container'>
                 <p className='m-0 text-center text-white'>
                     copyright &copy; Rupan 2024
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-    return (
-        <div>
-           <h1>Home Page</h1> 
-        </div>
-    );
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(
+        "https://ecomm-backend-dc9u.onrender.com/api/products/get"
+      );
+      setProducts(res.data.result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="container mt-4">
+      <div className="row g-4">
+        {products.map((ele) => (
+          <div className="col-md-4 col-sm-6" key={ele._id}>
+            <div className="card">
+              <div className="card-header text-center">
+                <h2>{ele.productName}</h2>
+              </div>
+              <img
+                src={ele.productImage}
+                className="card-img-top"
+                alt={ele.productName}
+              />
+              <div className="card-body text-center">
+                <h5 className="card-title">${ele.productPrice}</h5>
+              </div>
+              <div className="card-footer text-center">
+                <button type="button" className="btn btn-warning mx-1" >
+                  Edit
+                </button>
+                <button type="button" className="btn btn-danger mx-1" >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      </div>
+  );
 };
 
 export default Home;
